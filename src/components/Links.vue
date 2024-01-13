e6d4ba373ef57cd82fa7b3b4c486d1144a57059b
lowo <870050851@qq.com>
2024-01-14 04:04:10 +0800

    links

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -45,7 +45,7 @@ import { Icon } from "@vicons/utils";
 
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
 import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
-import { AngleRight, AddressBook } from "@vicons/fa"; // 注意使用正确的类别
+import { Bookmark, AddressBook } from "@vicons/fa"; // 注意使用正确的类别
 
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
@@ -73,7 +73,7 @@ const siteIcon = {
   Book,
   Fire,
   LaptopCode,
-  AngleRight,
+  Bookmark,
   AddressBook,
 };
 
